640b6c597430363250154937435a1824b1e39d36
sanya-shch <shchipkov.sanya@ukr.net>
2019-12-30 01:37:27 +0200

    frontend - registration

---
+++ client/src/hooks/http.hook.js
@@ -0,0 +1,41 @@
+import {useState, useCallback} from 'react';
+
+export const useHttp = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const request = useCallback(
+        async (url, method = 'GET', body = null, headers = {}) => {
+            setLoading(true);
+
+            try {
+                if(body){
+                    body = JSON.stringify(body);
+                    headers['Content-Type'] = 'application/json';
+                }
+
+                const res = await fetch(url, { method, body, headers });
+                const data = await res.json();
+
+                if (!res){
+                    throw new Error(data.message)
+                }
+
+                setLoading(false);
+
+                return data;
+            } catch (e) {
+                setLoading(false);
+                setError(e.message);
+                throw e;
+            }
+
+        },
+        []
+    );
+
+    // const clearError = () => setError(null);
+    const clearError = useCallback(() => setError(null), []);
+
+    return {loading, error, request, clearError};
+};
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {useHttp} from "../hooks/http.hook";
+import {useMessage} from "../hooks/message.hook";
 
 export const AuthPage = () => {
+    const message = useMessage();
+    const {loading, error, request, clearError} = useHttp();
+    const [value, setValue] = useState({email: '', password: ''});
+
+    useEffect(() => {
+        message(error);
+        clearError();
+    }, [error, message, clearError]);
+
+    const changeHandler = e => {
+        setValue({ ...value, [e.target.name]: e.target.value})
+    };
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'POST', {...value});
+            message(data.message);
+        } catch (e) {
+
+        }
+    };
+
     return (
-        <div>
-            <h1>Auth Page</h1>
+        <div className='row'>
+            <div className='col s6 offset-s3'>
+                <h1>Auth Page</h1>
+                <div className="card blue-grey darken-3">
+                    <div className="card-content white-text">
+                        <span className="card-title">Authorization</span>
+                        <div>
+                            <div className="input-field">
+                                <input onChange={changeHandler} id="email" type="email" name="email" className='white-text'/>
+                                    <label htmlFor="email" className='white-text'>Email</label>
+                            </div>
+                            <div className="input-field">
+                                <input onChange={changeHandler} id="password" type="password" name="password" className='white-text'/>
+                                    <label htmlFor="password" className='white-text'>Password</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-action">
+                        <button className='btn yellow darken-4' style={ {marginRight: 10} } disabled={loading}>Login</button>
+                        <button className='btn grey lighten-3 black-text' onClick={registerHandler} disabled={loading}>Registration</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
